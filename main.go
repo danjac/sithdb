234de05cfc7c07a94580ad14a94198de3b5d0aed
Dan Jacob <danjac354@gmail.com>
2015-12-13 01:07:30 +0200

    Parse and return sith

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 )
+
+type Sith struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	Homeworld  World  `json:"homeworld"`
+	Master     int    `json:"master"`
+	Apprentice int    `json:"apprentice"`
+}
+
+type World struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
 func main() {
 
@@ -14,6 +31,38 @@ func main() {
 		http.FileServer(http.Dir("./static"))))
 
 	render := render.New()
+
+	router.HandleFunc("/sith/{id}", func(w http.ResponseWriter, r *http.Request) {
+
+		id, err := strconv.Atoi(mux.Vars(r)["id"])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		f, err := ioutil.ReadFile("./data/sith.json")
+
+		if err != nil {
+			panic(err) // to be fixed
+		}
+
+		d := json.NewDecoder(bytes.NewReader(f))
+
+		var sithLords []Sith
+
+		if err := d.Decode(&sithLords); err != nil {
+			panic(err) // to be fixed
+		}
+
+		for _, sith := range sithLords {
+			if sith.ID == id {
+				render.JSON(w, http.StatusOK, sith)
+				return
+			}
+		}
+
+		http.Error(w, "No sith found for this ID", http.StatusNotFound)
+	})
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		render.HTML(w, http.StatusOK, "index", nil)
