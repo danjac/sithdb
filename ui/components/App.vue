485b353e4a9fb63cfe47ea6458dc5d81db0d8e69
Dan Jacob <danjac354@gmail.com>
2015-12-16 01:54:06 +0200

    Move view code into VM

--- ui/components/App.vue
+++ ui/components/App.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="app-container">
         <div class="css-root">
-            <h1 v-show="location" class="css-planet-monitor">Obi-Wan currently on {{location.name}}</h1>
+            <h1 v-if="location" class="css-planet-monitor">Obi-Wan currently on {{location.name}}</h1>
 
             <section class="css-scrollable-list">
               <ul class="css-slots">
-                <li :style="{ color: isSithOnWorld(slot) ? 'red': '' }" v-for="slot in slots" class="css-slot" track-by="$index">
+                <li :style="{ color: isSithHere(slot) ? 'red': '' }" v-for="slot in slots" class="css-slot" track-by="$index">
                   <h3 v-if="slot && slot.name">{{slot.name}}</h3>
                   <h6 v-if="slot && slot.homeworld">Homeworld: {{slot.homeworld.name}}</h6>
                 </li>
@@ -23,18 +23,27 @@
 <script>
 import store from '../store';
 
+let xhrRequests = [];
+
+function cancelRequests() {
+  for (var i=0; i < xhrRequests.length; i++) {
+    xhrRequests.shift().abort();
+  }
+}
+
 export default {
     name: "App",
     data() {
         return {
             location: null,
-            slots: [],
+            slots: [null, null, null, null, null],
+            isLoading: false,
+            isSithOnWorld: false,
+            isFirst: false,
+            isLast: false,
             buttonUpClass: 'css-button-up css-button-disabled',
             buttonDownClass: 'css-button-down css-button-disabled'
         };
-    },
-    ready() {
-        store.initialize(this.$http);
     },
     created() {
         store.on("update", this.update);
@@ -42,28 +51,123 @@ export default {
     destroyed() {
         store.removeListener("update", this.update);
     },
+    ready() {
+        this.fillSlots("down", 3616, 0);
+    },
     methods: {
-        update(event) {
-            this.location = store.getLocation();
-            this.slots = store.getSlots().slice();
-            this.buttonUpClass = 'css-button-up';
-            if (!store.canMoveUp()) {
-                this.buttonUpClass += ' css-button-disabled';
-            }
-            this.buttonDownClass = 'css-button-down';
-            if (!store.canMoveDown()) {
-                this.buttonDownClass += ' css-button-disabled';
+
+        canMoveUp() {
+          return !this.isFirst && !this.isSithOnWorld && !this.isLoading;
+        },
+
+        canMoveDown() {
+          return !this.isLast && !this.isSithOnWorld && !this.isLoading;
+        },
+        
+        fillSlots(direction, nextId, index) {
+
+          if (!nextId || this.isLoading) {
+            return;
+          }
+
+          this.isLoading = true;
+
+          this.$http.get("/sith/" + nextId, data => {
+
+            this.isLoading = false;
+            this.slots.splice(index, 1, data);
+
+            if (direction === "up") {
+              nextId = data.master;
+              index -= 1;
+            } else {
+              nextId = data.apprentice;
+              index += 1;
             }
 
+            this.isFirst = direction === "up" && !nextId;
+            this.isLast = direction === "down" && !nextId;
+
+            this.update();
+
+            if (nextId && this.slots[index] === null) {
+              this.fillSlots(direction, nextId, index);
+            }
+
+          }, {
+            beforeSend(req) {
+              xhrRequests.push(req);
+            }
+          });
         },
-        isSithOnWorld(slot) {
+
+        moveUp () {
+
+          if (!this.canMoveUp()) {
+            return;
+          }
+
+          cancelRequests();
+
+          let newSlots = this.slots.slice(0, 3);
+
+          const firstSlot = newSlots[0];
+          const nextId = firstSlot ? firstSlot.master : 0;
+
+          newSlots.splice(0, 0, null);
+          newSlots.splice(1, 0, null);
+
+          this.slots = newSlots.slice();
+          this.fillSlots("up", nextId, 1);
+
+        },
+
+        moveDown() {
+          if (!this.canMoveDown()) {
+            return;
+          }
+
+          cancelRequests();
+
+          let newSlots = this.slots.slice(2);
+          const lastSlot = newSlots[newSlots.length - 1];
+          const nextId = lastSlot ? lastSlot.apprentice : 0;
+
+          newSlots.splice(4, 0, null);
+          newSlots.splice(5, 0, null);
+
+          this.slots = newSlots.slice();
+          this.fillSlots("down", nextId, 3);
+        },
+
+        checkIfSithHomeworld() {
+          this.isSithOnWorld = _.some(this.slots, slot => {
+            return this.isSithHere(slot);
+          });
+          if (this.isSithOnWorld) {
+            cancelRequests();
+          }
+        },
+
+        update(event) {
+
+            this.location = store.getLocation();
+            this.checkIfSithHomeworld();
+
+            this.buttonUpClass = 'css-button-up';
+
+            if (!this.canMoveUp()) {
+                this.buttonUpClass += ' css-button-disabled';
+            }
+
+            this.buttonDownClass = 'css-button-down';
+
+            if (!this.canMoveDown()) {
+                this.buttonDownClass += ' css-button-disabled';
+            }
+        },
+        isSithHere(slot) {
             return slot && this.location && this.location.id === slot.homeworld.id;
-        },
-        moveUp(event) {
-            store.moveUp();
-        },
-        moveDown(event) {
-            store.moveDown();
         }
     }
 
--- ui/store.js
+++ ui/store.js
@@ -3,143 +3,14 @@ import { EventEmitter } from 'events';
 
 const store = new EventEmitter();
 
-let client = null;
-
 let location = null;
-let slots = [null, null, null, null, null];
-
-let xhrRequests = [];
-
-let isLoading = false;
-let isSithOnWorld = false;
-let isFirst = false;
-let isLast = false;
-
-function cancelRequests() {
-  for (var i=0; i < xhrRequests.length; i++) {
-    xhrRequests.shift().abort();
-  }
-}
-
-function checkIfSithHomeworld() {
-  isSithOnWorld = _.some(slots, slot => {
-    return slot && location && slot.homeworld.id === location.id;
-  });
-  if (isSithOnWorld) {
-    cancelRequests();
-  }
-
-}
-
-function fillSlots(direction, nextId, index) {
-
-  if (!nextId || isLoading) {
-    return;
-  }
-
-  isLoading = true;
-
-  client.get("/sith/" + nextId, data => {
-
-    isLoading = false;
-
-    slots[index] = data;
-
-    if (direction === "up") {
-      nextId = data.master;
-      index -= 1;
-    } else {
-      nextId = data.apprentice;
-      index += 1;
-    }
-
-    isFirst = direction == "up" && !nextId;
-    isLast = direction == "down" && !nextId;
-
-    checkIfSithHomeworld();
-
-    store.emit("update");
-
-    if (nextId && slots[index] === null) {
-      fillSlots(direction, nextId, index);
-    }
-
-  }, {
-    beforeSend(req) {
-      xhrRequests.push(req);
-    }
-  });
-}
-
-
-store.canMoveUp = () => {
-  return !isFirst && !isSithOnWorld && !isLoading;
-};
-
-store.canMoveDown = () => {
-  return !isLast && !isSithOnWorld && !isLoading;
-};
 
 store.getLocation = () => {
   return location;
-};
-
-store.getSlots = () => {
-  return slots;
-};
-
-
-store.moveUp = () => {
-  if (!store.canMoveUp()) {
-    return;
-  }
-
-  cancelRequests();
-
-  let newSlots = slots.slice(0, 3);
-
-  const firstSlot = newSlots[0];
-  const nextId = firstSlot ? firstSlot.master : 0;
-
-  newSlots.splice(0, 0, null);
-  newSlots.splice(1, 0, null);
-
-  slots = newSlots;
-  store.emit("update");
-
-  fillSlots("up", nextId, 1);
-};
-
-store.moveDown = () => {
-  if (!store.canMoveDown()) {
-    return;
-  }
-
-  cancelRequests();
-
-  let newSlots = slots.slice(2);
-  const lastSlot = newSlots[newSlots.length - 1];
-  const nextId = lastSlot ? lastSlot.apprentice : 0;
-
-  newSlots.splice(4, 0, null);
-  newSlots.splice(5, 0, null);
-
-  slots = newSlots;
-  store.emit("update");
-
-  fillSlots("down", nextId, 3);
-};
-
-
-store.initialize = (httpClient) => {
-  client = httpClient;
-  fillSlots("down", 3616, 0);
-};
-
+}
 
 store.updateLocation = (newLocation) => {
   location = newLocation;
-  checkIfSithHomeworld();
   store.emit("update");
 };
 
